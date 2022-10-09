cff25efa9ad2b4d9abd9acdc281bf5aba2b0e473
gouzuoluo <1046292959@qq.com>
2022-10-09 09:50:08 +0800

    first run

--- cmd/tkratos-realworld/wire_gen.go
+++ cmd/tkratos-realworld/wire_gen.go
@@ -7,30 +7,34 @@
 package main
 
 import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
 	"tkratos-realworld/internal/biz"
 	"tkratos-realworld/internal/conf"
 	"tkratos-realworld/internal/data"
 	"tkratos-realworld/internal/server"
 	"tkratos-realworld/internal/service"
-
-	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
 )
 
 // Injectors from wire.go:
 
-// wireApp init kratos application.
-func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*kratos.App, func(), error) {
-	dataData, cleanup, err := data.NewData(confData, logger)
+// initApp init kratos application.
+func initApp(confServer *conf.Server, confData *conf.Data, jwt *conf.JWT, logger log.Logger) (*kratos.App, func(), error) {
+	db := data.NewDB(confData)
+	dataData, cleanup, err := data.NewData(confData, logger, db)
 	if err != nil {
 		return nil, nil, err
 	}
-	greeterRepo := data.NewGreeterRepo(dataData, logger)
-	greeterUsecase := biz.NewUserUsecase(greeterRepo, logger)
-	greeterService := service.NewRealworldService(greeterUsecase)
-	grpcServer := server.NewGRPCServer(confServer, greeterService, logger)
-	httpServer := server.NewHTTPServer(confServer, greeterService, logger)
-	app := newApp(logger, grpcServer, httpServer)
+	userRepo := data.NewUserRepo(dataData, logger)
+	profileRepo := data.NewProfileRepo(dataData, logger)
+	userUsecase := biz.NewUserUsecase(userRepo, profileRepo, logger, jwt)
+	articleRepo := data.NewArticleRepo(dataData, logger)
+	commentRepo := data.NewCommentRepo(dataData, logger)
+	socialUsecase := biz.NewSocialUsecase(articleRepo, profileRepo, commentRepo, logger)
+	realworldService := service.NewRealworldService(userUsecase, socialUsecase, logger)
+	httpServer := server.NewHTTPServer(confServer, jwt, realworldService, logger)
+	grpcServer := server.NewGRPCServer(confServer, realworldService, logger)
+	app := newApp(logger, httpServer, grpcServer)
 	return app, func() {
 		cleanup()
 	}, nil
